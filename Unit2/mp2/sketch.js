c60eafaf62a476b1dc5ac0963fb8b487d26aed81
mksears <matts3ars@gmail.com>
2022-04-26 01:11:23 -0500

    mp2 final (hopefully)

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,46 +1,56 @@
-let space;
-let travel;
+let hyper;
+let stars;
 let arrive;
 let state = 0;
 let timer = 0;
 
-
-
 function setup() {
   createCanvas(600, 600);
-  space = loadImage("assets/state1.jpg");
-  travel = loadImage("assets/state 2.jpg");
-  arrive = loadImage("assets/state 3.jpg");
 
+  hyper = loadImage("assets/resting.jpg");
+  stars = loadImage("assets/flying.jpg");
+  arrive = loadImage("assets/arrive.jpg");
 }
 
 function draw() {
-  background(600);
+
+  background(255);
 
   switch (state) {
 
     case 0:
-      image(space , 0, 0, 600, 600);
+      image(hyper, 0, 0, 600, 600);
+
 
       break;
 
     case 1:
-      image(travel, 0, 0, 600, 600);
+      image(stars, 0, 0, 600, 600);
 
-      if (timer > 3*60){
-        timer = 0 ;
-        state = 2 ;
+      //timer code
+      timer++ ;
+      if(timer > 3*60) {
+        timer = 0;
+        state = 2;
       }
       break;
 
     case 2:
       image(arrive, 0, 0, 600, 600);
 
+      //timer code
+      timer++ ;
+      if(timer > 5*60) {
+        timer = 0;
+        state = 0;
+      }
       break;
-  }
-
-  function mouseReleased() {
-    state++;
-    if (state > 2) state = 0;
 
   }
+}
+
+function mouseReleased() {
+  state++;
+  if (state > 2) state = 0;
+
+}
